fe91becff9dbca8fbe7fff029c0d6d50ff76ff8c
Eric <ericemeka732@gmail.com>
2025-08-14 05:19:15 +0100

    Routing specification fixed

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,19 @@ export default function HomePage() {
   const role = user?.role ?? "guest";
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-blue-950 cursor-pointer text-white px-4">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Restaurant Management</h1>
+      <h1 className="text-3xl font-bold mb-4">
+        Welcome to Restaurant Management
+      </h1>
       <p className="mb-6 max-w-md text-center">
-        Manage your restaurant efficiently with role based dashboards for Admin, Cashier, and Waiter.
+        Manage your restaurant efficiently with role based dashboards for Admin,
+        Cashier, and Waiter.
       </p>
 
       {role === "admin" && (
         <>
-          <p className="mb-4">You have full admin access to manage everything.</p>
+          <p className="mb-4">
+            You have full admin access to manage everything.
+          </p>
           <button
             onClick={() => router.push("/admin/dashboard")}
             className="bg-white text-blue-900  cursor-pointer px-6 py-3 rounded-md font-semibold hover:bg-gray-100 transition"
@@ -39,6 +44,28 @@ export default function HomePage() {
             className="bg-white text-blue-900 cursor-pointer px-6 py-3 rounded-md font-semibold hover:bg-gray-100 transition"
           >
             Go to Cashier Dashboard
+          </button>
+        </>
+      )}
+      {role === "cook" && (
+        <>
+          <p className="mb-4">You can manage orders and payments.</p>
+          <button
+            onClick={() => router.push("/kitchen")}
+            className="bg-white text-blue-900 cursor-pointer px-6 py-3 rounded-md font-semibold hover:bg-gray-100 transition"
+          >
+            Go to Cook Dashboard
+          </button>
+        </>
+      )}
+      {role === "chef" && (
+        <>
+          <p className="mb-4">You can manage orders and payments.</p>
+          <button
+            onClick={() => router.push("/kitchen")}
+            className="bg-white text-blue-900 cursor-pointer px-6 py-3 rounded-md font-semibold hover:bg-gray-100 transition"
+          >
+            Go to Chef Dashboard
           </button>
         </>
       )}
